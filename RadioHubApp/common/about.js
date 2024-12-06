dafae6bc0caac5f64ef137785d8f5796a242e982
ElijahW1117 <xftyip@gmail.com>
2024-12-06 16:26:31 +0800

    about update

--- RadioHubApp/common/about.js
+++ RadioHubApp/common/about.js
@@ -4,11 +4,11 @@ window.addEventListener("scroll", function () {
     const banner = document.getElementById("banner");
     const scrollPosition = window.scrollY;
 
-    if (scrollPosition > 700 - 90 && !isBannerFixed) {
+    if (scrollPosition > 400 - 90 && !isBannerFixed) {
         // 當滾動超過切換位置且尚未固定時
         banner.classList.add("fixed");
         isBannerFixed = true;
-    } else if (scrollPosition <= 700 - 90 && isBannerFixed) {
+    } else if (scrollPosition <= 400 - 90 && isBannerFixed) {
         // 當滾回頂部且已經固定時
         banner.classList.remove("fixed");
         isBannerFixed = false;
