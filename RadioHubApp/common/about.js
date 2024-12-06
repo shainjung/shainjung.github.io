3e251980026c2c6af77f6541c08920f152a1c8a3
ElijahW1117 <xftyip@gmail.com>
2024-12-06 12:59:49 +0800

    about update

--- RadioHubApp/common/about.js
+++ RadioHubApp/common/about.js
@@ -14,3 +14,24 @@ window.addEventListener("scroll", function () {
         isBannerFixed = false;
     }
 });
+
+// 取得來源與目標元素
+const s1 = document.getElementById("s1");//sourceDiv
+const t1 = document.getElementById("t1");//targetDiv
+const s2 = document.getElementById("s2");//sourceDiv
+const t2 = document.getElementById("t2");//targetDiv
+const s3 = document.getElementById("s3");//sourceDiv
+const t3 = document.getElementById("t3");//targetDiv
+
+// 監聽 window 的滾動事件
+window.addEventListener("scroll", () => {
+    // 取得來源 <div> 的 top 值 (相對於視窗)
+    const sourceTop1 = s1.getBoundingClientRect().top;
+    const sourceTop2 = s2.getBoundingClientRect().top;
+    const sourceTop3 = s3.getBoundingClientRect().top;
+
+    // 將來源 <div> 的 top 值設定到目標 <div>
+    t1.style.top = `${window.scrollY + sourceTop1}px`;
+    t2.style.top = `${window.scrollY + sourceTop2}px`;
+    t3.style.top = `${window.scrollY + sourceTop3}px`;
+});
